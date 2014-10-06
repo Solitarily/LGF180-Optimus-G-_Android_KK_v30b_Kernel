ff7bf0bbf1a9581879693be15d4c7463ad3facb9
Solitarily <chuangweida@gmail.com>
2014-10-06 20:46:54 +0800

    This is LGF180k30b Stock kernel kitkat4.4

---
+++ Readme-build.txt
@@ -0,0 +1,36 @@
+中文
+1. Kernel 编译 
+  - 安装内核压缩工具
+    a) apt-get install lzop
+
+  - 在Android的目录中使用以下命令解压
+        tar xvzf LGF180_G_Kitkat_v30a_kernel.tar.gz
+      
+  - 编译内核源代码，你必须添加谷歌的工具链(toolchain) 进入Android的目录.
+
+  - 运行以下的脚本来构建内核 
+    a) cd kernel
+    b) ./build-gku.sh
+
+  - 建立之后，就可以找到生成的zip文件。 在Packages/g-kernel-dev-××-××-××××.zip
+
+  - 拷贝到手机SD卡进行刷机，替换当前内核。
+
+English
+1. Kernel Build 
+  - Installing the kernel compression tools
+    a) apt-get install lzop
+
+  - Uncompress using following command at the android directory
+        tar xvzf LGF180_G_Kitkat_V30a_kernel.tar.gz 
+       
+  - When you compile the kernel source code, you have to add google original prebuilt source(toolchain) into the android directory.
+
+  - Run following scripts to build kernel
+    a) cd kernel
+    b) ./build-gku.sh
+  
+  - After the establishment, you can find the generated zip file. In Packages/g-kernel-dev-××-××-××××.zip
+
+  - Copy to the phone SD card brush machine, replace the current kernel.
+   
---
+++ build-gku.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+
+#### FOR DEVELOPING ONLY DOES NOT CONTAIN 99kernel INIT SCRIPT TO CONFIG THE KERNEL. ASSUMES YOU ARE DOING A DIRTY FLASH ####
+
+## time start ##
+
+time_start=$(date +%s.%N)
+
+# Number of jobs (usually the number of cores your CPU has (if Hyperthreading count each core as 2))
+MAKE="8"
+
+
+## Set compiler location to compile with linaro cortex a8
+echo "Setting compiler location..."
+export PATH=$PATH:tools/lz4demo
+export ARCH=arm
+export CROSS_COMPILE=../../../prebuilts/gcc/linux-x86/arm/arm-eabi-4.7/bin/arm-eabi-
+
+## Build kernel using rider_defconfig
+make gku_bcm-perf_defconfig
+make -j`grep 'processor' /proc/cpuinfo | wc -l` ARCH=arm
+sleep 1
+
+# Post compile tasks
+echo "Copying compiled kernel and modules to Packages/out/"
+echo "and building flashable zip"
+sleep 1
+
+     mkdir -p Packages/
+     mkdir -p Packages/out/
+     mkdir -p Packages/out/system/lib/modules/
+     mkdir -p Packages/out/tmp/
+     mkdir -p Packages/out/tmp/kernel/
+     mkdir -p Packages/out/META-INF/
+
+cp -a $(find . -name *.ko -print |grep -v initramfs) Packages/out/system/lib/modules/
+cp -rf prebuilt-scripts/META-INF/ Packages/out/
+cp -rf prebuilt-scripts/tmp/ Packages/out/
+cp arch/arm/boot/zImage Packages/out/tmp/kernel/
+
+# build flashable zip
+     export curdate=`date "+%m-%d-%Y"`
+     cd Packages/out/
+     zip -r ../g-kernel-dev-$curdate.zip .
+     echo "Deleting Temp files and folders...."
+     cd ../../
+     rm -rf Packages/out/
+
+echo "Build Complete, Check Packages directory for flashable zip"
+time_end=$(date +%s.%N)
+echo -e "${BLDYLW}Total time elapsed: ${TCTCLR}${TXTGRN}$(echo "($time_end - $time_start) / 60"|bc ) ${TXTYLW}minutes${TXTGRN} ($(echo "$time_end - $time_start"|bc ) ${TXTYLW}seconds) ${TXTCLR}"
+
+
